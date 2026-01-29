378ab51142b02d7633321ff34da539444d40bd8a
Baber19 <Baber19i@outlook.com>
2026-01-29 13:47:52 +0500

    Facebook Ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Facebook Login Page Ui',
+      title: 'Facebook',
       theme: ThemeData(
         // This is the theme of your application.
         //
